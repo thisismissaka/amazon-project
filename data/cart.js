7257825deb42fb90a7cc829dd342d327a752ec1d
Missaka Ranasinghe <missaka.ranasinghe@outlook.com>
2025-03-16 13:36:05 +0530

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,12 @@
-export const cart = [];
+export const cart = [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+},
+{
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1
+}
+];
 
 export function addToCart(productId){
 
@@ -6,9 +14,9 @@ export function addToCart(productId){
     const quantity = Number(selectValue.value);
     let matchingItem;
   
-    cart.forEach((item)=>{
-      if(productId===item.productId){
-        matchingItem = item;
+    cart.forEach((cartItem)=>{
+      if(productId===cartItem.productId){
+        matchingItem = cartItem;
       }
     });
   
